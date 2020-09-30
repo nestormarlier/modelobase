abe89d4014ecaed18c27d0419a8182d71de759e6
nestormarlier <47480842+nestormarlier@users.noreply.github.com>
2020-09-30 14:13:53 -0300

    template

--- modelBaseApp/views.py
+++ modelBaseApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    data = {
+        'title': 'Página de inicio'
+    }
+    return render(request, 'index.html',data)
