d2de1dd9ae9cd21ce4436a6c14e73e5bc5e42557
nestormarlier <47480842+nestormarlier@users.noreply.github.com>
2020-10-03 16:38:20 -0300

    categoria/list.html

--- modelBaseApp/urls.py
+++ modelBaseApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import inicio
+from .views import inicio, categoria
 
 urlpatterns = [
     path('home/', inicio, name='Home'),
+    path('categorias/', categoria, name='Categorias'),
 ]
--- modelBaseApp/views.py
+++ modelBaseApp/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from .models import Categoria
+
 # Create your views here.
+
 
 def inicio(request):
     data = {
         'title': 'Página de inicio'
     }
-    return render(request, 'index.html',data)
+    return render(request, 'index.html', data)
+
+def categoria(request):
+    data = {
+        'title': 'Listado de categorías',
+        'categorias': Categoria.objects.all(),
+    }
+    return render(request, 'categorias/list.html', data)
